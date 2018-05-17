ce66308cae556e5762b7d89175d10508d852b11c
Nicholas Reed <nreed@fusionalliance.com>
2018-05-17 16:07:43 -0400

    Adding simple example of MSTest test project built against .net core
    Moved from local machine into source control.

---
+++ NetCoreLibraryPlayground/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace NetCoreLibraryPlayground
+{
+    [TestClass]
+    public class Class1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            throw new Exception("Failed Test!");
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Console.WriteLine("TestMethod2 Pass!");
+            Microsoft.VisualStudio.TestTools.UnitTesting.Logging.Logger.LogMessage("TestMethod2 Pass from VS Unit Testing Logging");
+        }
+    }
+}
